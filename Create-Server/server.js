e0c2eb1317223f8408445a69da5d07b837a34c1f
kanishkasubash <kanishkasubash@gmail.com>
2023-11-02 01:29:08 +0530

    modified:   Create-Server/package-lock.json 	modified:   Create-Server/package.json 	modified:   Create-Server/server.js

--- Create-Server/server.js
+++ Create-Server/server.js
@@ -1,10 +1,20 @@
 /**
  * Create Express Server
- * localhost:3000
+ * localhost:8000
  */
 const express = require("express");
 const db = require("./database");
 const app = express();
+
+const cors = require("cors");
+app.use(
+    cors({
+        exposedHeaders: ["Content-Length", "X-Foo", "X-Bar"],
+        credentials: true,
+        origin: "*"
+    })
+);
+
 const PORT = 8000;
 
 app.listen(PORT, () => {
@@ -14,11 +24,28 @@ app.listen(PORT, () => {
 /* HTTP GET Method*/
 app.get("/api/products", (req, res, next) => {
     // Code to retrive data
-    
+    try {
+        const sql = "SELECT * FROM products";
+        const params = [];
+        db.all(sql, params, (err, rows) => {
+            if (err) {
+                res.status(400).json({
+                    error: err.message
+                });
+                return;
+            }
+            res.status(200).json({
+                message: "success",
+                data: rows,
+            });
+        });
+    } catch (e) {
+        res.status(400).send(e);
+    }
 });
 
 /* HTTP POST Method*/
 app.post("/api/products", (req, res, next) => {
     // Code to insert data
-    
+
 });
